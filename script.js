96d8792841aec5a714516465ba6926659f293d5f
Muhammad Ahmed <muhammadahmed12590@gmail.com>
2026-07-07 11:19:10 -0700

    bottom to top button

--- script.js
+++ script.js
@@ -17,6 +17,28 @@ navLinks.forEach((link) => {
 });
 
 
+
+// bottom to top // 
+
+  const backToTop = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
+
  
 
 
@@ -96,3 +118,9 @@ const reviewTrack = document.querySelector(".review-track");
   createDots();
   updateReviewSlider();
  
+
+
+
+
+
+
